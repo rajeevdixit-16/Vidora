74880562203bc1f39d31bcda004db6eb4769295e
rajeev-45 <dixitrajeev5202@gmail.com>
2025-07-01 22:00:47 +0530

    new

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ app.use(express.static("public"))  //serves static files
 
 app.use(cookieParser())
 
+// Routes
+
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter)
+// http://localhost:8000/api/v1/users/register
+
 
 
 export { app }
